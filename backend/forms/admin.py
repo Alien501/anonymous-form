c969a494c73566c0138ec57540cc9ef718657519
Alien501 <cvignesh404@gmail.com>
2025-08-09 09:15:37 +0530

    Added forms

--- backend/forms/admin.py
+++ backend/forms/admin.py
@@ -5,8 +5,49 @@ from django.contrib.admin import ModelAdmin
 from .models import *
 
 # Register your models here.
+
+class FormQuestionInline(admin.TabularInline):
+    model = FormQuestion
+    extra = 1
+    fields = ['question', 'form_index']
+    readonly_fields = ['form_index', 'created_at', 'updated_at']
+
 @admin.register(Form)
 class FormAdmin(ModelAdmin):
+    compressed_fields = True
     list_display = ['name',]
     search_fields = ['name',]
     list_filter = ['roles',]
+    inlines = [FormQuestionInline]
+    fieldsets = (
+        ('Form Details', {'fields': ('id', 'name','enable')}),
+        ('Form Configuration', {'fields': ('roles', 'department', 'group')}),
+        ('Form Meta', {'fields': ('created_at', 'updated_at')}),
+    )
+    readonly_fields = ['created_at', 'updated_at', 'id']
+    
+@admin.register(Questions)
+class QuestionsAdmin(ModelAdmin):
+    list_display = ['id', 'question', 'answer_type']
+    search_fields = ['question']
+    list_filter = ['answer_type']
+    
+    fieldsets = (
+        ('Question', {'fields': ('id', 'question', 'required')}),
+        ('Answer', {'fields': ('answer_type',)}),
+        ('Question Meta', {'fields': ('created_at', 'updated_at')})
+    )
+    
+    readonly_fields = ['id', 'created_at', 'updated_at']
+    
+@admin.register(FormQuestion)
+class FormQuestionAdmin(ModelAdmin):
+    list_display = ['form', 'question', 'form_index']
+    search_fields = ['form', 'question']
+    
+    fieldsets = (
+        ('Form Question', {'fields': ('id', 'form', 'question', 'form_index')}),
+        ('Meta', {'fields': ('created_at', 'updated_at')})
+    )
+    
+    readonly_fields = ['id', 'form_index', 'created_at', 'updated_at']
--- backend/forms/models.py
+++ backend/forms/models.py
@@ -1,14 +1,57 @@
 from django.db import models
-
 from organisation.models import *
+
+from time import timezone
 
 import uuid
 
 # Create your models here.
 class Form(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    enable = models.BooleanField("Enable", default=False)
     name = models.CharField("Form Name", max_length=255)
     roles = models.ManyToManyField(Role)
     department = models.ManyToManyField(Department)
     group = models.ManyToManyField(Group)
     
+    created_at = models.DateTimeField("Created At", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated At", auto_now=True)
+    
+class Questions(models.Model):
+    ANSWER_TYPES = (
+        ('text', 'Text'),
+        ('number', 'Number'),
+        ('radio', 'Radio'),
+        ('checkbox', 'Checkbox'),
+        ('select', 'Select'),
+        ('file', 'File'),
+    )
+    
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    question = models.CharField("Question", max_length=2000, null=False, blank=False)
+    required = models.BooleanField("Required", default=False)
+    answer_type = models.CharField("Answer Type", max_length=30, choices=ANSWER_TYPES, default='text')
+    created_at = models.DateTimeField("Created At", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated At", auto_now=True)
+    
+class FormQuestion(models.Model):
+    id = models.UUIDField(default=uuid.uuid1, editable=False, primary_key=True, unique=True)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    form_index = models.IntegerField("Form Index", default=0)
+    created_at = models.DateTimeField("Created At", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated At", auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        if self.form_index is None or self.form_index == 0:
+            query = FormQuestion.objects.filter(form=self.form)
+            if self.pk:
+                query = query.exclude(pk=self.pk)
+            
+            last_index = query.aggregate(
+                models.Max('form_index')
+            )['form_index__max']
+            
+            self.form_index = (last_index or 0) + 1
+        
+        super().save(*args, **kwargs)
