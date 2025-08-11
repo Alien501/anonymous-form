5fefb94adb84c6660a0f6db603b237df94756ed2
Alien501 <cvignesh404@gmail.com>
2025-08-12 00:16:26 +0530

    Roouting fix

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { createHashHistory, createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 
 import { DefaultCatchBoundary } from "~/components/default-catch-boundary";
@@ -27,6 +27,7 @@ export function createRouter() {
     defaultNotFoundComponent: DefaultNotFound,
     scrollRestoration: true,
     defaultStructuralSharing: true,
+    history: createHashHistory(),
   });
 
   setupRouterSsrQueryIntegration({
